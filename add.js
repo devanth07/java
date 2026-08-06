467d94fcc876bae6f420ec3753b2ebfa32704e3a
devanth07 <devanthjamu@gmail.com>
2026-08-06 08:58:14 +0530

    Fix formatting and improve input/output in add.js

--- add.js
+++ add.js
@@ -2,5 +2,15 @@ let a = parseInt(prompt("Enter first integer:"), 10);
 let b = parseInt(prompt("Enter second integer:"), 10);
 console.log("Sum = " + (a + b));
 
-8
-9
+output:
+Enter first integer:5
+Enter second integer:6
+Sum = 11
+
+Enter first integer:50
+Enter second integer:6
+Sum = 56
+
+Enter first integer:10
+Enter second integer:11
+Sum = 21
