47a23d826c2e9a78e1074afe546d88a277cbba94
devanth07 <devanthjamu@gmail.com>
2026-08-06 08:43:13 +0530

    Add digit counting functionality for an integer

---
+++ count of integer.js
@@ -0,0 +1,9 @@
+let num = parseInt(prompt("Enter an integer:"), 10);
+let digitCount = Math.abs(num).toString().length;
+
+console.log("Number of digits = " + digitCount);
+
+
+output:
+Enter an integer:22111
+Number of digits = 5
