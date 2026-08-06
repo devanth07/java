84324f8097a85cf50d2c27c35f65fc3e8b21d01a
devanth07 <devanthjamu@gmail.com>
2026-08-06 09:03:24 +0530

    Document output examples for reverse string
    Added output examples for the reverse string function.

--- reverse string.js
+++ reverse string.js
@@ -3,3 +3,12 @@ let reversed = str.split("").reverse().join("");
 
 console.log("Reversed string = " + reversed);
 
+output:
+Enter a string:harikant
+Reversed string = tnakirah
+
+Enter a string:devanth
+Reversed string = htnaved
+
+Enter a string:full stack 
+Reversed string = kcats lluf
