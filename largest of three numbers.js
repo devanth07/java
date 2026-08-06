ae2d55df2daa4507c18c17bc7bdbc2c9c81e97a0
devanth07 <devanthjamu@gmail.com>
2026-08-06 09:00:34 +0530

    Update and rename largest of three numbers to largest of three numbers.js

--- largest of three numbers.js
+++ largest of three numbers.js
@@ -1,7 +1,6 @@
 let a = parseInt(prompt("Enter first number:"), 10);
 let b = parseInt(prompt("Enter second number:"), 10);
 let c = parseInt(prompt("Enter third number:"), 10);
-
 let largest = a;
 if (b > largest) {
   largest = b;
@@ -11,8 +10,19 @@ if (c > largest) {
 }
 console.log("Largest = " + largest);
 
-output:8
-7
-9
+output:
+Enter first number:8
+Enter second number:5
+Enter third number:9
+Largest = 9
 
-Largest=9
+Enter first number:8
+Enter second number:3
+Enter third number:2
+Largest = 8
+
+Enter first number:3
+Enter second number:5
+Enter third number:12
+Largest = 12
+
